8de418558f4825316aa7794f4ab35b60d3c0b98b
Philip Holm <philipbaltzersen@gmail.com>
2026-04-12 14:28:43 +0200

    Autosave file in nvim before running tests

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -10,5 +10,31 @@ return {
         ["neotest-vitest"] = {},
       },
     },
+    keys = {
+      {
+        "<leader>tt",
+        function()
+          vim.cmd("write")
+          require("neotest").run.run(vim.fn.expand("%"))
+        end,
+        desc = "Run File (Neotest)",
+      },
+      {
+        "<leader>tr",
+        function()
+          vim.cmd("write")
+          require("neotest").run.run()
+        end,
+        desc = "Run Nearest (Neotest)",
+      },
+      {
+        "<leader>tl",
+        function()
+          vim.cmd("write")
+          require("neotest").run.run_last()
+        end,
+        desc = "Run Last (Neotest)",
+      },
+    },
   },
 }
